b2fc6844283f032a62b08d55e845931e8862bdf3
Jatin Satani <jatinsatani111@gmail.com>
2024-02-10 12:40:39 +0530

    Done

---
+++ lecture practice/tuplelistextendfunction.py
@@ -0,0 +1,8 @@
+def tuple_extend(tuple1, tuple2):
+    return tuple1 + tuple2
+
+
+tuple1 = (1, 2, 3)
+tuple2 = (4, 5, 6)
+extended_tuple = tuple_extend(tuple1, tuple2)
+print("Extended Tuple:", extended_tuple)
