8564007fea46a0dfa171e50bbfb94a9ae21fbf7d
Jatin Satani <jatinsatani111@gmail.com>
2024-02-13 15:10:44 +0530

    Donessss

--- lecture practice/tuplelistextendfunction.py
+++ lecture practice/tuplelistextendfunction.py
@@ -4,4 +4,4 @@ def tuple_extend(tuple1, tuple2):
 tuple1 = (1, 2, 3)
 tuple2 = (4,)
 extended_tuple = tuple_extend(tuple1, tuple2)
-print("Extended Tupless:", extended_tuple)
+print("Extended Tuple:", extended_tuple)
