b768f2b1c8d3826fe908698b6b65530c900b8252
Jatin Satani <jatinsatani111@gmail.com>
2024-02-15 15:09:40 +0530

    bh

--- lecture practice/filedemo.py
+++ lecture practice/filedemo.py
@@ -1,2 +1,13 @@
-import os
-os.makedirs("myfile")
+# exception demo 
+
+from builtins import int
+while True:
+    try:
+        n=input("Enter the value : ")
+        n=int(n)
+        break
+    except Exception:
+        print("Invalid Input")
+    finally:
+        print("Finally Called")
+print("Bye")
