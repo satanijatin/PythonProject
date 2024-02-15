cf4a9b1482b1c4dba2946b6e3c5a87752234f4b7
Jatin Satani <jatinsatani111@gmail.com>
2024-02-15 15:52:37 +0530

    Done

---
+++ Assignment3/program6.py
@@ -0,0 +1,9 @@
+filename = 'C:/Users/JATIN/Documents/GitHub/PythonProject/Assignment3/example.txt'
+n = int(input("Enter line you want : "))
+try:
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        for line in lines[-n:]:
+            print(line.strip())
+except FileNotFoundError:
+    print("File not found.")
