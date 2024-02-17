6f6b03760f45aaa937d1ee32fca6578d1b82c4ea
Jatin Satani <jatinsatani111@gmail.com>
2024-02-17 14:04:43 +0530

    Done

---
+++ lecture practice/classexample/multipleinhertiance.py
@@ -0,0 +1,31 @@
+class A:
+
+    def __init__(self):
+        print("Hello world")
+
+    def displaya(self):
+        print("first class is A")
+
+class B():
+
+    def __init__(self):
+        print("hello")
+    
+
+    def displayb(self):
+        print("Second class is B")
+
+class C(A,B):
+
+    def __init__(self):
+        print("hello C")
+    
+
+    def displayc(self):
+        print("Third class is C")
+
+c=C()
+c.displaya()
+c.displayb()
+c.displayc()
+        
